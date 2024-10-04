563861aa3a4ef9ad7c6dbf5fcafff64e944829cd
Patrick Lynch <patricklynch15@yahoo.com>
2024-10-04 12:10:33 -0400

    commit prior to removing grid element

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import { SimpleGrid, GridItem } from "@chakra-ui/react";
+import { SimpleGrid, GridItem, Image } from "@chakra-ui/react";
+import { useState } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import initialImage from "./Images/15B9D2AD-219B-442E-9DF8-FEF33DF5C2AD.jpeg";
+import defaultBackground from "./Images/IMG_6678.jpeg";
+import Background from "./Components/Background";
 
 function App() {
   const menuItems = ["Home", "Gallery", "Book Session", "More"];
+  const [background, setBackground] = useState(`${defaultBackground}`);
 
   return (
     <SimpleGrid
-      bgImage={`${initialImage}`}
       templateAreas={`"header header header header"
         "main main main main"
         "footer footer footer footer"`}
-      gridTemplateRows={"50px 1fr 50px"}
+      gridTemplateRows={"50px 1fr 25px"}
       minHeight={"max-content"}
-      color="blackAlpha.700"
-      fontWeight="bold"
       columns={4}
+      backgroundImage={defaultBackground}
+      backgroundSize={"cover"}
+      flexDir={"row-reverse"}
+      backgroundRepeat={"no-repeat"}
     >
-      <GridItem pl="2" bg="orange.300" area={"header"}>
+      <GridItem pl="2" bg="red.300" area={"header"} alignContent={"flex-start"}>
         <NavBar menuItems={menuItems} />
       </GridItem>
-      <GridItem pl="2" area={"main"} height={"700px"}></GridItem>
-      <GridItem pl="2" bg="blue.300" area={"footer"}>
-        Footer
+      <GridItem pl="2" area={"main"} height={"700px"}>
+        <p id="CentralText"> Here's some sample text</p>
       </GridItem>
+      <GridItem pl="2" area={"footer"} bg="blue.300" />
     </SimpleGrid>
   );
 }
